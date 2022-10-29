315fcfae87192a17a5dda844eb41de6d085ef90d
elonehoo <huchengyea@163.com>
2022-10-29 11:59:28 +0800

    fix:add core method

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-export const one = 1
-export const two = 2
+import isObject from "@elonehoo/is-object"
+import isWindow from "@elonehoo/is-window"
+
+function isDom(val:any) {
+	if (!isObject(val) || !isWindow(window) || !(`Node` in window)) {
+		return false
+	}
+
+	return typeof val.nodeType === `number`
+		&& typeof val.nodeName === `string`
+}
+
+export default isDom

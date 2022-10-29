1280a3a0a7a713c2b7955be8fc220a0f487bdeca
elonehoo <huchengyea@163.com>
2022-10-29 12:25:35 +0800

    test:there are tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,23 @@
 import { describe, expect, it } from 'vitest'
+import isDom from '~/index'
 
+// @vitest-environment happy-dom
 describe('should', () => {
   it('exported', () => {
-    expect(1).toEqual(1)
+    expect(isDom(null)).toEqual(false)
+    expect(isDom(false)).toEqual(false)
+    expect(isDom(new Date())).toEqual(false)
+    expect(isDom([])).toEqual(false)
+    expect(isDom(2)).toEqual(false)
+    expect(isDom(`bar`)).toEqual(false)
+    expect(isDom(/asda/)).toEqual(false)
+    expect(isDom({})).toEqual(false)
+
+    expect(isDom({ nodeType: 1, nodeName: `BODY` })).toEqual(true)
+    expect(isDom(document.createElement(`body`))).toEqual(true)
+    expect(isDom(window)).toEqual(false)
+    expect(isDom(document)).toEqual(true)
+
+
   })
 })
